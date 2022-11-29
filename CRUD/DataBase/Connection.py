3b590028656b19f5409d2652dfb6bd8ad030e776
John <yhonelvis49@gmail.com>
2022-11-29 17:29:11 -0400

    subiendo archivos modificados

--- CRUD/DataBase/Connection.py
+++ CRUD/DataBase/Connection.py
@@ -1,4 +1,5 @@
 import pymysql
+import os
 
 class Connection:
     def __init__(self):
@@ -9,3 +10,17 @@ class Connection:
             database = 'agenda'
         )
         self.cursor = self.connection.cursor()
+    def InsertarEvento(self,idAgenda,idPersona,temaReunion,lugar,fecha,hora):
+        sql = "insert into reunion (idAgenda,idPersona,temaReunion,lugar,fecha,hora) values({},{},'{}','{}','{}','{}')".format(idAgenda,idPersona,temaReunion,lugar,fecha,hora)
+        self.cursor.execute(sql)
+        print("Ingresado correctamente ")
+        self.cursor.connection.commit()
+        os.system('pause')
+        os.system('cls')
+        
+    def InsertarPersona(self,idPersona,nombre,apellido,ci):
+        sql=("insert into persona (idPersona,nombre,apellido,ci) values({},'{}','{}',{})".format(idPersona,nombre,apellido,ci))
+        self.cursor.execute(sql)
+        self.cursor.connection.commit()
+        os.system('pause')
+        os.system('cls')
