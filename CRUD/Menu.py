4b04aebd6ae118585ed33053513fcdc1f9b52fa4
luisTrr <drax026@gmail.com>
2022-12-01 12:20:07 -0400

    añadiendo el menu update a la clase menu

--- CRUD/Menu.py
+++ CRUD/Menu.py
@@ -35,6 +35,20 @@ class Menu:
     ================================
     ''',self.colores.RESET)
       
+  def menuUpdate(self):
+    print(self.colores.amarillo,'''
+    ================================
+        ---->>>> EDITAR <<<<----
+      [1] Nombre de Persona 
+      [2] CI de Persona
+      [3] Tema de reunion
+      [4] Lugar de ruinion
+      [5] Fecha de reunion
+      [6] Hora de reunion
+      [0] Atras
+    ================================
+    ''',self.colores.RESET)
+    
   def menuDelete(self):
     print(self.colores.rojo,'''
     ================================
